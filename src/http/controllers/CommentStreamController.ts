eeaa9edb0936b770d04f509b0005949f7c144345
Alexzvn <personal@alexzvn.me>
2023-12-28 14:44:26 +0700

    add some of history

--- src/http/controllers/CommentStreamController.ts
+++ src/http/controllers/CommentStreamController.ts
@@ -1,5 +1,16 @@
 import { Stream } from '@elysiajs/stream'
 import { emitter } from './FeedController'
+import dayjs, { Dayjs } from 'dayjs'
+
+let buffer = new Array<Record<string, any>&{ time: Dayjs }>()
+
+emitter.on('message', comment => {
+  buffer.push({ ...comment, time: dayjs() })
+})
+
+setInterval(() => {
+  buffer = buffer.filter(value => value.time.subtract(1, 'minute').isBefore(dayjs()))
+}, 10 * 1000)
 
 app.get('/api/feed/comment/subscribe', ({ set }) => {
 
@@ -7,6 +18,10 @@ app.get('/api/feed/comment/subscribe', ({ set }) => {
   set.headers['Access-Control-Allow-Methods'] = '*'
 
   return new Stream(stream => {
+    for (const item of buffer) {
+      stream.send(JSON.stringify({ ...item, time: undefined }))
+    }
+
     emitter.on('message', body => stream.send(JSON.stringify(body)))
   })
 })
