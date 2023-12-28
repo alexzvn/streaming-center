9f28989216ee37a7709464a76dd11789452836fe
Alexzvn <personal@alexzvn.me>
2023-12-28 14:04:42 +0700

    wip

--- src/http/controllers/CommentStreamController.ts
+++ src/http/controllers/CommentStreamController.ts
@@ -4,6 +4,7 @@ import { emitter } from './FeedController'
 app.get('/api/feed/comment/subscribe', ({ set }) => {
 
   set.headers['Access-Control-Allow-Origin'] = '*'
+  set.headers['Access-Control-Allow-Methods'] = '*'
 
   return new Stream(stream => {
     emitter.on('message', body => stream.send(JSON.stringify(body)))
